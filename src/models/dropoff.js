a6feb012730e5b16e62ab94a12da874259b6b00a
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2024-10-16 20:28:51 +0100

    updated drop-off schema

--- src/models/dropoff.js
+++ src/models/dropoff.js
@@ -2,11 +2,23 @@ const mongoose = require("mongoose");
 
 const dropOffSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  distance: { type: mongoose.Schema.Types.String },
-  img: { type: mongoose.Schema.Types.String, required: true },
+  distance: { type: Number }, // Will be calculated dynamically
+  distanceUnit: { type: String, default: "miles" },
+  img: { type: String, required: true },
   wasteType: [
     { type: mongoose.Schema.Types.ObjectId, ref: "Wastetype", required: true },
   ],
+  location: {
+    type: {
+      type: String, // Always 'Point'
+      enum: ["Point"], // 'location.type' must be 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number], // [longitude, latitude]
+      required: true,
+    },
+  },
 });
 
 const DropOff = mongoose.model("DropOff", dropOffSchema);
