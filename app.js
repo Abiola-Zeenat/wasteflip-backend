680d068b03b6e0cec4cb82d495ca958d95083db4
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2024-10-19 17:22:59 +0100

    Added wastetype controller and routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./src/config/db");
+const wasteRouter = require("./src/routes/wastetype")
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cookieParser());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/wastetype", wasteRouter);
 
 //Database connection to the server before starting the server
 const port = process.env.PORT || 3005;
