6baecf0ceb3275628107ba3ee677f7e3fddd5731
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2024-10-23 21:49:14 +0100

    updated schedule schema

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -2,22 +2,29 @@ const mongoose = require("mongoose");
 
 const scheduleSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  enquiryType: { type: String, required: true },
-  date: { type: Date, required: true, default: Date.now() },
-  size: { type: String },
-  quantity: { type: String, required: true },
-  frequency: { type: String, required: true },
-  wasteType: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Wastetype",
-    required: true,
-  },
+  scheduleItems: [
+    {
+      wasteType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Wastetype",
+        required: true,
+      },
+      date: { type: Date, required: true, default: Date.now() },
+      size: { type: String },
+      quantity: { type: String, required: true },
+      frequency: { type: String, required: true },
+    },
+  ],
   contact: {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    phoneNumber: { type: String },
+    phoneNumber: { type: String, required: true },
     email: { type: String },
+    address: { type: String, required: true },
   },
+  subTotal: { type: Number, required: true },
+  serviceCharge: { type: Number, required: true },
+  Total: { type: Number, required: true },
   status: {
     type: String,
     enum: ["In-progress", "completed"],
